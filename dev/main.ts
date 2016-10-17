b72ebfce9c9b78abf2929df826c8709a910f957d
Sumit Maitra <code@sumitmaitra.com>
2016-10-17 21:13:32 +0100

    added favicon

--- dev/main.ts
+++ dev/main.ts
@@ -4,18 +4,13 @@ import nconf = require('nconf');
 import { Configuration } from "./server/services/settings/config-model";
 import { Config } from "./server/services/settings/config";
 import * as express from "express";
-//import { Container } from "./server/di/container";
-//import { CrossRouter } from "./server/services/routing/cross-router";
 
-// var express = require('express');
 var multer = require('multer');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
+
 var fs = require('fs');
 var app: express.Application = express();
 var http = require('http');
@@ -24,15 +19,10 @@ var configService = new Config();
 
 try {
     configService.load((config: Configuration) => {
-        // let credentials = {
-        //     key: fs.readFileSync(__dirname + config.key, 'utf8'),
-        //     cert: fs.readFileSync(__dirname + config.cert, 'utf8')
-        // };
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: false }));
 
         // Register routes
-
         app.use('/.well-known', express.static(__dirname + '/www/.well-known')); //static route for Letsncrypt validation
         app.use(express.static(__dirname + '/www')); // All static stuff from /app/wwww
 
@@ -66,9 +56,6 @@ try {
         httpServer.listen(3002, (): void => {
             console.log(pkg.name, 'listening on port ', httpServer.address().port);
         });
-        // httpsServer.listen(3445, (): void => {
-        //     console.log(pkg.name, 'listening on port ', httpsServer.address().port);
-        // });
     });
 }
 catch (err) {
