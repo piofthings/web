248d3661dc0f75494c3d3a3310636b283a7e4e6d
Sumit Kumar Maitra <sumitkm@gmail.com>
2017-09-27 23:44:43 +0100

    The blog is coming

---
+++ dev/_blog/hello-world.md
@@ -0,0 +1,19 @@
+{{{
+  "title": "Hello World!",
+  "tags": ["blog", "fun"],
+  "category": "random",
+  "date": "7-29-2017",
+  "author": "Sumit Maitra"
+}}}
+
+Well, that took a while, but the blog is finally up.
+
+Setting up a Blog is a usually a piece of cake if you want it to be, but then, where's the fun in that right?
+
+So I've taken the really long route and set it up from scratch. It doesn't use any CMS system. Instead it uses a tiny
+but beautiful (in my eyes) library called ```Poet```. I had to [fork it](https://github.com/piofthings/poet) because the [original library](http://jsantell.github.io/poet) looks like it needs some love.
+
+I will write more about ```Poet``` in my software oriented [blog](http://sumitmaitra.wordpress.com).  
+I shall keep this place solely for IoT projects, Robotics and 3D Printing related stuff only.  
+
+Okay, back to work now, need that atom/rss feed running for some robots competition ;-)
--- dev/main.ts
+++ dev/main.ts
@@ -1,11 +1,11 @@
-/// <reference path="./typings/index.d.ts"/>
-
-import nconf = require('nconf');
+import * as nconf from 'nconf';
 import { Configuration } from "./server/services/settings/config-model";
 import { Config } from "./server/services/settings/config";
 import * as express from "express";
+import * as multer from "multer";
+import * as pug from "pug";
+var Poet = require("poet");
 
-var multer = require('multer');
 var favicon = require('serve-favicon');
 var session = require('express-session');
 var bodyParser = require('body-parser');
@@ -21,37 +21,58 @@ try {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: false }));
 
-        // Register routes
+        var poet = Poet(app, {
+            postsPerPage: 3,
+            posts: __dirname + '/_blog',
+            metaFormat: 'json',
+            routes: {
+                '/blog/:post': 'post',
+                '/blog/:page': 'page',
+                '/tags/:tag': 'tag',
+                '/categories/:category': 'category'
+            }
+        });
+
+        poet.init((err: any, cb: any)=>{
+            console.log("Err" + JSON.stringify(err));
+        }).then(
+            (data: any, cb: any) => {
+            console.log("Initialized");
+            let posts = poet.helper.getPosts(0,5);
+            console.log(JSON.stringify(posts, null, 1))
+        });
+
+        app.set('view engine', 'pug');
+        app.set('views', __dirname + '/views');
+        app.get('/blog', (req, res) => {
+            res.render('index');
+        });
+        app.get('/pages', (req, res) => {
+            res.render('index');
+        });
+        app.get('/tags', (req, res) => {
+            res.render('index');
+        });
+        app.get('/categories', (req, res) => {
+            res.render('index');
+        });
+
+        app.get('/rss', function (req, res) {
+          // Only get the latest posts
+          var posts = poet.helpers.getPosts(0, 5);
+          res.setHeader('Content-Type', 'application/rss+xml');
+          res.render('rss', { posts: posts });
+        });
+        
         app.use('/.well-known', express.static(__dirname + '/www/.well-known')); //static route for Letsncrypt validation
         app.use(express.static(__dirname + '/www')); // All static stuff from /app/wwww
 
         // catch 404 and forward to error handler
-        app.use(function(req: Express.Request, res: Express.Response, next: any) {
-            var err = new Error('Not Found');
-            err.message = "404";
-            next(err.message + ": Unhandled Error");
-        });
 
-        // error handlers
-        // development error handler
-        // will print stacktrace
-        if (app.get('env') === 'development') {
-            app.use((err: any, req: express.Request, res: express.Response, next: any) => {
-                res.status(err.status || 500);
-                next(err.status || 500 + ": Unhandled Error" + err.message);
-            });
-        }
 
-        // production error handler
-        // no stacktraces leaked to user
-        app.use((err: any, req: express.Request, res: express.Response, next: any) => {
-            res.status(err.status || 500);
-            next(err.status || 500 + ": Unhandled Error");
-        });
 
         var pkg = require('./package.json');
         var httpServer = http.createServer(app);
-        // var httpsServer = https.createServer(credentials, app);
         httpServer.listen(3002, (): void => {
             console.log(pkg.name, 'listening on port ', httpServer.address().port);
         });
